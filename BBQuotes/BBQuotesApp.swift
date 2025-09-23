aab973dbd77b47123755581691fe2fbeaa6368fb
dawoodakbar <dawood.3techsoft@gmail.com>
2025-09-23 15:02:51 +0500

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Dawood on 23/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
