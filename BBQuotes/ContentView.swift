27637f12e0b3cbc5d456a2d25fdb7c6bd44675fb
DawoodAkbar <uzumakiminato806@gmail.com>
2025-09-26 10:12:35 +0500

    Added custom colors for Breaking Bad, and added the fetch button, with some ui fix

--- BBQuotes/ContentView.swift
+++ BBQuotes/ContentView.swift
@@ -12,13 +12,11 @@ struct ContentView: View {
         TabView {
             Tab("Breaking Bad", systemImage: "tortoise") {
                 QuoteView(show: "Breaking Bad")
-                Text("Breaking Bad View")
                     .toolbarBackgroundVisibility(.visible, for: .tabBar)
             }
             
             Tab("Better Call Saul", systemImage: "briefcase") {
                 QuoteView(show: "Better Call Saul")
-                Text ("Better Call Saul View")
                     .toolbarBackgroundVisibility(.visible, for: .tabBar)
             }
         }
