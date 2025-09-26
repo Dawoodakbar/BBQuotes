7852a3867a23edf11d8056faabba40567e2c1434
DawoodAkbar <uzumakiminato806@gmail.com>
2025-09-26 11:52:47 +0500

    Added the fetchButton and passed the fetchFunc to get the quotes, and added custom colors for both buttons

---
+++ BBQuotes/CharacterView.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterView.swift
+//  BBQuotes
+//
+//  Created by Dawood on 26/09/2025.
+//
+
+import SwiftUI
+
+struct CharacterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharacterView()
+}
