2801a1824f21a224c3eaeab21a389232606f7162
Dawood Akbar <uzumakiminato806@gmail.com>
2025-09-28 19:08:32 +0500

    Added Disclouser group for spoiler... and added the display name

--- BBQuotes/CharacterView.swift
+++ BBQuotes/CharacterView.swift
@@ -13,68 +13,97 @@ struct CharacterView: View {
     
     var body: some View {
         GeometryReader { geo in
-            ZStack(alignment: .top) {
-                Image(show.lowercased().replacingOccurrences(of: " ", with: ""))
-                    .resizable()
-                    .scaledToFit()
-                
-                ScrollView {
-                    TabView {
-                        ForEach(character.images, id: \.self) { characterImageURL in
-                            AsyncImage(url: characterImageURL) { image in
-                                image
-                                    .resizable()
-                                    .scaledToFill()
-                            } placeholder: {
-                                ProgressView()
+            ScrollViewReader { proxy in
+                ZStack(alignment: .top) {
+                    Image(show.lowercased().replacingOccurrences(of: " ", with: ""))
+                        .resizable()
+                        .scaledToFit()
+                    
+                    ScrollView {
+                        TabView {
+                            ForEach(character.images, id: \.self) { characterImageURL in
+                                AsyncImage(url: characterImageURL) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFill()
+                                } placeholder: {
+                                    ProgressView()
+                                }
                             }
                         }
-                    }
-                    .tabViewStyle(.page)
-                    .frame(width: geo.size.width/1.2, height: geo.size.height/1.7)
-                    .clipShape(.rect(cornerRadius: 25))
-                    .padding(.top, 60)
-                    
-                    VStack(alignment: .leading) {
-                        Text(character.name)
-                            .font(.largeTitle)
+                        .tabViewStyle(.page)
+                        .frame(width: geo.size.width/1.2, height: geo.size.height/1.7)
+                        .clipShape(.rect(cornerRadius: 25))
+                        .padding(.top, 60)
                         
-                        Text("Portrayed By: \(character.portrayedBy)")
-                            .font(.subheadline)
-                        
-                        Divider()
-                        
-                        Text("\(character.name) Character Info")
-                            .font(.title2)
-                        
-                        Text("Born: \(character.birthday)")
-                        
-                        Divider()
-                        
-                        Text("Occupation")
-                        
-                        ForEach(character.occupations, id: \.self) { occupation in
-                            Text("⦿ \(occupation)")
+                        VStack(alignment: .leading) {
+                            Text(character.name)
+                                .font(.largeTitle)
+                            
+                            Text("Portrayed By: \(character.portrayedBy)")
                                 .font(.subheadline)
-                        }
-                        
-                        Divider()
-                        
-                        Text("Nicknames:")
-                        
-                        if character.aliases.count > 0 {
-                            ForEach(character.aliases, id: \.self) { aliase in
-                            Text("⦿ \(aliase)")
+                            
+                            Divider()
+                            
+                            Text("\(character.name) Character Info")
+                                .font(.title2)
+                            
+                            Text("Born: \(character.birthday)")
+                            
+                            Divider()
+                            
+                            Text("Occupation")
+                            
+                            ForEach(character.occupations, id: \.self) { occupation in
+                                Text("⦿ \(occupation)")
                                     .font(.subheadline)
                             }
-                        } else {
-                            Text("None")
-                                .font(.subheadline)
+                            
+                            Divider()
+                            
+                            Text("Nicknames:")
+                            
+                            if character.aliases.count > 0 {
+                                ForEach(character.aliases, id: \.self) { aliase in
+                                    Text("⦿ \(aliase)")
+                                        .font(.subheadline)
+                                }
+                            } else {
+                                Text("None")
+                                    .font(.subheadline)
+                            }
+                            
+                            Divider()
+                            
+                            DisclosureGroup("Status (spoiler alert!):") {
+                                VStack(alignment: .leading) {
+                                    Text(character.status)
+                                        .font(.title2)
+                                    
+                                    if let death = character.death {
+                                        AsyncImage(url: death.image) {image in
+                                            image
+                                                .resizable()
+                                                .scaledToFit()
+                                                .clipShape(.rect(cornerRadius: 15))
+                                        } placeholder: {
+                                            ProgressView()
+                                        }
+                                        
+                                        Text("How: \(death.details)")
+                                            .padding(.bottom, 7)
+                                        
+                                        Text("Last Words: \"\(death.lastWords)\"")
+                                    }
+                                }
+                            }
                         }
+                        .padding(.bottom, 50)
+                        .frame(width: geo.size.width/1.25, alignment: .leading)
+                        .id(1)
                     }
-                    .frame(width: geo.size.width/1.25, alignment: .leading)
+                    .scrollIndicators(.hidden)
                 }
-                .scrollIndicators(.hidden)
             }
         }
         .ignoresSafeArea()
